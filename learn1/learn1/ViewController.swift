6e0a51c54853e76fb0305a1663e155c9ee2b09de
tsuhadaiki <33239441+tsuhadaiki@users.noreply.github.com>
2019-02-15 18:39:58 +0900

    pop-upと出席管理を追加した。（発表用）

--- learn1/learn1/ViewController.swift
+++ learn1/learn1/ViewController.swift
@@ -2,13 +2,16 @@
 //  ViewController.swift
 //  learn1
 //
-//  Created by 玉城翔平 on 2019/02/10.
+//
 //  Copyright © 2019 Swift-Beginners. All rights reserved.
 //
-
 import UIKit
-
+import UserNotifications
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    @IBOutlet weak var buttan: UIButton!
+    @IBOutlet weak var label: UILabel!
+    let userDefaults = UserDefaults.standard
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return todonakami.count
     }
@@ -16,24 +19,62 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let todocell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "todocell",for: indexPath)
         todocell.textLabel!.text = todonakami[indexPath.row]
-    
-     return todocell
+        
+        return todocell
     }
-    
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         let index = indexPath.row
         todonakami.remove(at: index)
         tableView.reloadData()
-         UserDefaults.standard.set(todonakami, forKey: "todolist")
+        UserDefaults.standard.set(todonakami, forKey: "todolist")
     }
     
-     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // 通知を作成
+    @IBAction func setNotification(_ sender: Any) {
+        // タイトル、本文、サウンド設定の保持
+        let content = UNMutableNotificationContent()
+        content.title = "設定された時間です"
+        content.subtitle = "5秒経過しました"
+        content.body = "なんか書いとけ"
+        content.sound = UNNotificationSound.default
         
+        // seconds後に起動するトリガーを保持
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        
+        // 識別子とともに通知の表示内容とトリガーをrequestに内包する
+        let request = UNNotificationRequest(identifier: "Timer", content: content, trigger: trigger)
+        
+        // UNUserNotificationCenterにrequestを加える
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self as? UNUserNotificationCenterDelegate
+        center.add(request) { (error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    var text1: String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
         if UserDefaults.standard.object(forKey: "todolist") != nil{
             todonakami = UserDefaults.standard.object(forKey: "todolist") as! [String]
         }
+        //buttan.setTitle("+", for: .normal)
+       // label.text = readData()
+        
+        // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    
+    func readData() -> String {
+        let str: String = userDefaults.object(forKey:"lecture") as! String
+        return str
+    }
+    // フォアグラウンドの場合でも通知を表示する
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .badge, .sound])
+    }
+    
 }
-
